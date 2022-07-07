31e8455d907bc97b63aa921721dfbada9c64ffd1
icoo08217 <icoo08216@gmail.com>
2022-07-07 15:41:15 +0900

    등록 기능 구현 시작

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -12,10 +12,27 @@ public class App {
         while(true) {
             System.out.printf("명령) ");
             String cmd = sc.nextLine().trim();
+            int index = 1;
 
             switch (cmd) {
                 case "종료":
                     break outer;
+
+                case "등록":
+                    System.out.printf("명언 : ");
+                    String content = sc.nextLine().trim();
+
+                    System.out.printf("작가 : ");
+                    String author = sc.nextLine().trim();
+
+                    System.out.println( index + "번 명언이 등록되었습니다.\n");
+                    break;
+                case "목록":
+                    break;
+                case "수정":
+                    break;
+                case "삭제":
+                    break;
             }
         }
         sc.close();
