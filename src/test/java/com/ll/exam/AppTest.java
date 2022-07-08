3f920e50dd994d73b57276e5319097cd71bc4d27
icoo08217 <icoo08216@gmail.com>
2022-07-08 13:50:20 +0900

    Junit Test 기능 동작하는지 확인

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -2,10 +2,13 @@ package com.ll.exam;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AppTest {
 
     @Test
     public void 태스트_실험() {
-
+        int result = 10 + 20;
+        assertEquals(30, result);
     }
 }
